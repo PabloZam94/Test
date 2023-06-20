30f612fadda49a60e40daf9ccf619f8396096c21
Pablo <pablo_soa_08@outlook.com>
2023-06-20 16:02:21 -0600

    Delete comment code

---
+++ Test/Utils/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  Test
+//
+//  Created by pablo luis velazquez zamudio on 19/06/23.
+//
+
+import Foundation
+
+class Constats {
+    static let sitieID  = "MLM"
+    static let baseHost = "api.mercadolibre.com"
+}
